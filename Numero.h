2c844eff7c4d7b7be97a2dc21ed989a44119502b
Leonardo <leonardborj@gmail.com>
2017-09-01 13:26:25 -0600

    Comienzo

---
+++ Numero.h
@@ -0,0 +1,16 @@
+#ifndef NUMERO_H
+#define NUMERO_H
+#include <string>
+using namespace std;
+
+class Numero{
+	private:
+		string numero;
+		public:
+		Numero();
+		Numero(string);
+		string getNumero();
+		void getNumero();
+};
+#endif
+
